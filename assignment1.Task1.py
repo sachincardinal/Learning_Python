09afc741044b6b6318db234b810234da19e6f425
sachin mishra <sachin.mishra10@gmail.com>
2025-05-21 16:00:40 +0530

    Assignment1 , Task1 and 2 Commit

---
+++ assignment1.Task1.py
@@ -0,0 +1,14 @@
+num1= int(input("Enter First Number"))
+num2= int(input("Enter Second Number"))
+
+sum=num1+num2
+diff=num1 - num2
+multi=num1* num2
+div=num1/num2
+
+print(sum)
+print(diff)
+print(multi)
+print(div)
+
+
---
+++ assignment1.Task2.py
@@ -0,0 +1,9 @@
+num1= input("Enter Your First Name : ")
+num2= input("Enter Your Last  Name : ")
+
+full_name = first_name + " " + last_name
+
+# Step 3: Print personalized greeting message
+print(f"Hello, {full_name}! Welcome!")
+
+
