415ec6f23a85e5b7e60b4dbdf5e79788de421138
sachin mishra <iammishrasachin@gmail.com>
2026-06-08 14:14:38 +0530

    ASSIGNMENT 4: Task1

--- assignment4.Module5.Task1.py
+++ assignment4.Module5.Task1.py
@@ -5,5 +5,6 @@ try:
         for line_number, line in enumerate(file, start=1):
             print(f"Line {line_number}: {line.strip()}")
 
+
 except FileNotFoundError:
     print("Error: The file 'sample.txt' was not found.")
